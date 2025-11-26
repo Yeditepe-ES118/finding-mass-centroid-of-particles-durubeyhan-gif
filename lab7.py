d380fcb9934cc75d887bd891b2a315760b97244b
durubeyhan-gif <duru.beyhan@std.yeditepe.edu.tr>
2025-11-26 10:49:11 +0300

    Update lab7.py

--- lab7.py
+++ lab7.py
@@ -7,7 +7,7 @@ def centroid(p1x,p1y,p2x,p2y,p3x,p3y,m1,m2,m3):
     cy= np.sum(positions[1,:]*mass)/np.sum(mass)
     tot_mass= np.sum(mass)
     
-    return cx, cy, tot_mass
+    return tot_mass, cx, cy
 #ilk olarak pozisyonları tanımlıyoruz
 #istediğin yerde durmak için kırmızı noktayı işaretle ve debug file ile kodu çalıştır
 #cx= p1xm1+p2xm2+p3xm3/m1+m2+m3
