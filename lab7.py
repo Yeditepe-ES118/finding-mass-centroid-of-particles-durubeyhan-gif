4ce0487cf0df237b06ca4cb58ead19d7f21c9397
durubeyhan-gif <duru.beyhan@std.yeditepe.edu.tr>
2025-11-26 10:38:42 +0300

    Add files via upload

---
+++ lab7.py
@@ -0,0 +1,15 @@
+import numpy as np
+def centroid(p1x,p1y,p2x,p2y,p3x,p3y,m1,m2,m3):
+    positions= np.array([[p1x,p2x,p3x], [p1y,p2y,p3y]])
+    mass= np.array ([m1,m2,m3])
+    
+    cx= np.sum(positions[0,:]*mass)/ np.sum(mass) # * elementwise multiplication
+    cy= np.sum(positions[1,:]*mass)/np.sum(mass)
+    tot_mass= np.sum(mass)
+    
+    return cx, cy, tot_mass
+#ilk olarak pozisyonları tanımlıyoruz
+#istediğin yerde durmak için kırmızı noktayı işaretle ve debug file ile kodu çalıştır
+#cx= p1xm1+p2xm2+p3xm3/m1+m2+m3
+# [0,:] birinci satır anlamına geliyor
+result= centroid(1,2,3,4,5,1,1,1,1)
